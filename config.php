533bea93dc9158dbba65d5aa2c9a9c9bedc1981e
Kirill Chudinov <cayce252@gmail.com>
2012-09-27 21:25:46 +0200

    Added README file and added some comments in config

--- config.php
+++ config.php
@@ -27,17 +27,17 @@ $passwords[1] = isset($passwords[1])?trim($passwords[1]):'';
     // Database connection data for localhost, and not a local host.
 if($_SERVER['SERVER_ADDR'] == '127.0.0.1')
 {
-    define('DBMS_NAME','mysql');  // Database management system name
-    define('DB_HOST'  ,'localhost');
-    define('DB_PORT'  ,'3306');
-    define('DB_USER'  ,'root');
-    define('DB_PASS'  ,$passwords[0]);
-    define('DB_DATABASENAME','Boomboom');
-    define('DB_DATABASENAME_LOG','log');
+    define('DBMS_NAME','mysql');          // Database management system name
+    define('DB_HOST'  ,'localhost');      // If the server is on the same ip as mysql(or other dbms), the host is localhost
+    define('DB_PORT'  ,'3306');           // Default port for mysql
+    define('DB_USER'  ,'root');           // Name of the user with access to Boomboom and log databases
+    define('DB_PASS'  ,$passwords[0]);    // Password of the user 
+    define('DB_DATABASENAME','Boomboom'); // Name of the main database
+    define('DB_DATABASENAME_LOG','log');  // Name of database for log error
 }
 else
 {
-    define('DBMS_NAME','mysql');  // Database management system name
+    define('DBMS_NAME','mysql');
     define('DB_HOST'  ,'localhost');
     define('DB_PORT'  ,'3306');
     define('DB_USER'  ,'user475_boomboom');
