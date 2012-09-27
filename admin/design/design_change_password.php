35263206f85d6e7756110fc7a291c622df34e01d
Kirill Chudinov <cayce252@gmail.com>
2012-09-27 22:23:54 +0200

    Corrected action in form

--- admin/design/design_change_password.php
+++ admin/design/design_change_password.php
@@ -12,7 +12,7 @@ elseif($change_password->error == 2) $change_password->error = 'Password incorre
 <center>
 <?php echo !empty($change_password->error)?'<span class="label label-important">'.$change_password->error.'</span>':''; ?>
 <?php echo $change_password->success?'<span class="label label-success">You changed your password</span>':''; ?><br><br>
-<form action="Change_Password" method="POST">
+<form action="ChangePassword" method="POST">
 old pass: <input type="password" name="old_pass" /><br>
 new pass: <input type="password" name="new_pass" /><br>
 <button class="btn btn-primary" name="login_submit" type="submit">Change</button>
