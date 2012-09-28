33490fe2bb9993590e49674e050a50864ee62232
Kirill Chudinov <cayce252@gmail.com>
2012-09-28 13:38:04 +0200

    Edited required files from js bootstrap

--- admin/design/design_bottom.php
+++ admin/design/design_bottom.php
@@ -4,12 +4,10 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
      <script src="design/static/js/autoloadjobs.js"></script>
-     <script src="design/bootstrap/js/jquery.js"></script>
-     <script src="design/bootstrap/js/bootstrap-alert.js"></script>
-     <script src="design/bootstrap/js/autoloadjobs.js"></script>
-     <script src="design/bootstrap/js/bootstrap-tab.js"></script>
-     <script src="design/bootstrap/js/bootstrap-dropdown.js"></script>
-     <script src="design/bootstrap/js/bootstrap-collapse.js"></script>
+     <script src="design/static/bootstrap/js/jquery.js"></script>
+     <script src="design/static/bootstrap/js/bootstrap-alert.js"></script>
+     <script src="design/static/bootstrap/js/bootstrap-tab.js"></script>
+     <script src="design/static/bootstrap/js/bootstrap-collapse.js"></script>
 
   </body>
 </html>
