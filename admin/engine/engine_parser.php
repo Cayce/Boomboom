9de6d4f52fda228e6b90a7e6697479d6e567aa09
Udi Oron <udioron@gmail.com>
2013-10-17 13:18:00 +0200

    DRY
    http://en.wikipedia.org/wiki/Don't_repeat_yourself

--- admin/engine/engine_parser.php
+++ admin/engine/engine_parser.php
@@ -10,19 +10,32 @@ if(!isset($_SESSION['admin']))
     die;
 }
 
-require 'engine/classes/parser/class_parser_webmaster.php';
-require 'engine/classes/parser/class_parser_themarker.php';
-require 'engine/classes/parser/class_parser_jobnet.php';
-require 'engine/classes/parser/class_parser_jobmax.php';
-require 'engine/classes/parser/class_parser_alljobs.php';
-require 'engine/classes/parser/class_parser_jobzone.php';
-require 'engine/classes/parser/class_parser_avodazmanit.php';
-require 'engine/classes/parser/class_parser_jobmaster.php';
-require 'engine/classes/parser/class_parser_homeless.php';
-require 'engine/classes/parser/class_parser_freelancerim.php';
-require 'engine/classes/parser/class_parser_drushim.php';
+# DRY: http://en.wikipedia.org/wiki/Don't_repeat_yourself
 
-    // Make an array with all the websites
+# unchecked code, probably buggy
+
+$parsers = array(
+    'webmaster',
+    'themarker',
+    'jobnet',
+    'jobmax',
+    'alljobs',
+    'jobzone',
+    'avodazmanit',
+    'jobmaster',
+    'homeless',
+    'freelancerim',
+    'drushim',
+);
+
+$classes = array();
+foreach ($parsers as $parser) {
+    require "engine/classes/parser/class_parser_$parser.php";
+    $func = "parser_$parser";
+    $classes[$parser] = $func;
+}
+
+// Make an array with all the websites
 require 'engine/classes/class_jobs.php';
 $jobs = new jobs();
 $jobs->get_websites();
@@ -34,73 +47,10 @@ if(isset($_POST['parse_submit']))
 {  
     set_time_limit(0);
     
-    if(isset($_POST['websites'][2]))
+    foreach($_POST['websites'] as $parser) { // be sure to set <input name="jobinfo">
     {
-        $parser_alljobs = new parser_alljobs();
-        $parser_alljobs->parse();
+        $p = new $classes[$parser]();
+        $p->parse();
     }
-    
-    if(isset($_POST['websites'][5]))
-    {
-        $parser_themarker = new parser_themarker();
-        $parser_themarker->parse();
-    }
-    
-    if(isset($_POST['websites'][10]))
-    {
-        $parser_jobnet = new parser_jobnet();
-        $parser_jobnet->parse();
-    }
-    
-    if(isset($_POST['websites'][13]))
-    {
-        $parser_jobmax = new parser_jobmax();
-        $parser_jobmax->parse();
-    }
-    
-    if(isset($_POST['websites'][7]))
-    {
-        $parser_jobzone = new parser_jobzone();
-        $parser_jobzone->parse();
-    }
-    
-    if(isset($_POST['websites'][8]))
-    {
-        $parser_avodazmanit = new parser_avodazmanit();
-        $parser_avodazmanit->parse();
-    }
-    
-    if(isset($_POST['websites'][9]))
-    {
-        $parser_jobmaster = new parser_jobmaster(); //there is a captcha !!! 18.05.13: а самому исправить не судьба?! спассибо бля.
-        $parser_jobmaster->parse();
-    }
-    
-    if(isset($_POST['websites'][11]))
-    {
-        $parser_homeless = new parser_homeless();
-        $parser_homeless->parse();
-    }
-    
-    if(isset($_POST['websites'][4]))
-    {
-        $parser_freelancerim = new parser_freelancerim();
-        $parser_freelancerim->parse();
-    }
-    
-    if(isset($_POST['websites'][21]))
-    {
-        $parser_drushim = new parser_drushim();
-        $parser_drushim->parse();
-    }
-    
-    if(isset($_POST['websites'][3]))
-    {
-        $parser_webmaster = new parser_webmaster();
-        $parser_webmaster->parse();
-    }
-    
-    db::query("UPDATE `parser` SET `last_id`=`last_id`+1 WHERE `name`='parser'");
-}
 
 require 'design/design_parser.php';
